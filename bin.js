4694581f121fe3d4ef0608c5f7aae50a8d924248
jlord <jessica.lord@github.com>
2013-11-16 12:05:34 -0800

    fixed CL build

--- bin.js
+++ bin.js
@@ -2,11 +2,9 @@
 
 var args = process.argv.slice(2)
 var modules = process.argv.slice(2)
-var extend   = require('lodash.assign')
-
-var Sheetsee = {}
-
-module.exports = Sheetsee
+var extend = require('lodash.assign')
+var browserify = require('browserify')
+var through = require('through')
 
 makeBuildInstructions(modules, includeModules)
 
@@ -17,24 +15,35 @@ function makeBuildInstructions(modules, cb) {
 // write an err into this function
 function getWantedModules(modules, cb) {
   var npmModules = []
+  if (modules === "") console.log("Please include modules -maps, -tables, -charts")
   modules.forEach(function whichModules(module) {
-      if (module === "-m" || module === "-maps") npmModules.push("sheetsee-maps") && console.log("I want maps!")
-      if (module === "-t" || module === "-tables") npmModules.push("sheetsee-tables") &&  console.log("I want tables!")
-      if (module === "-c" || module === "-charts")  npmModules.push("sheetsee-charts") && console.log("I want charts!")
+      if (module === "-m" || module === "-maps") return npmModules.push("sheetsee-maps") 
+      if (module === "-t" || module === "-tables") return npmModules.push("sheetsee-tables") 
+      if (module === "-c" || module === "-charts") return npmModules.push("sheetsee-charts")
+      console.error( module + " doesn't exist, please use -maps, -tables or -charts")
   })
   cb(npmModules)
 }
 
 function includeModules(npmModules) {
-  console.log("I ran include Modules", npmModules)
-  var extendString = "extend(Sheetsee, "
+  if (npmModules.length === 0) return console.error("Aborted build, no modules required")
+  var extendString = "if (typeof Sheetsee === 'undefined') window.Sheetsee = {};"
+    + "var extend = require('lodash.assign'); extend(Sheetsee, "
   var counter = npmModules.length
   npmModules.forEach(function addModules(module) {
-    console.log("counter is at", counter)
     counter--
     if (counter != 0) extendString = extendString + "require('" + module + "'), "
     if (counter === 0) extendString = extendString + "require('" + module + "'))"
-    console.log("extend is", extendString)
   })
-  console.log(extend)
+  // run the build
+  runBuild(extendString)
 }
+
+function runBuild(extendString) {
+  var dataStream = through()
+  b = browserify()
+  b.files.push(dataStream)
+  b.bundle().pipe(process.stdout)
+  dataStream.queue(extendString)
+  dataStream.queue(null)
+  }
